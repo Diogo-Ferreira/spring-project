c8d64d9d1fd31c7429aa6c4dc6312f552c3f6b75
SylvainRamseyer <sylvain.ramseyer@gmail.com>
2017-03-23 14:29:13 +0100

    model for the Trip table

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -2,6 +2,10 @@ package application;
 
 import java.util.Arrays;
 
+import application.entities.Trip;
+import application.repositories.TripRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -10,28 +14,47 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
+
 @SpringBootApplication
 public class Application {
 
 
 
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+    public CommandLineRunner commandLineRunner(TripRepository repository) {
         return args -> {
+            /*repository.save(new Trip("sextrip", "Bogota"));
 
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
+            // fetch all customers
+            log.info("Customers found with findAll():");
+            log.info("-------------------------------");
+            for (Trip trip : repository.findAll()) {
+                log.info(trip.toString());
             }
+            log.info("");
 
+            // fetch an individual customer by ID
+            Trip trip = repository.findOne(1L);
+            log.info("Customer found with findOne(1L):");
+            log.info("--------------------------------");
+            log.info(trip.toString());
+            log.info("");
+
+            // fetch customers by last name
+            log.info("Customer found with findByLastName('Bauer'):");
+            log.info("--------------------------------------------");
+            for (Trip bauer : repository.findByName("sextrip")) {
+                log.info(bauer.toString());
+            }
+            log.info("");
+            */
         };
     }
 
--- src/main/java/application/repositories/TripRepository.java
+++ src/main/java/application/repositories/TripRepository.java
@@ -1,11 +1,14 @@
 package application.repositories;
 
-import application.entities.TripEntity;
+import application.entities.Trip;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * Created by diogo on 3/10/17.
  */
-public interface TripRepository extends CrudRepository<TripEntity, Long> {
+public interface TripRepository extends CrudRepository<Trip, Long> {
+    List<Trip> findByName(String name);
 
 }
