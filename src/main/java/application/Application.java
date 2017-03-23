626bd745893a9003b5d16853e6d5a64a4326783c
LumenH <aurelie.debrot@he-arc.ch>
2017-03-23 14:52:59 +0100

    template shit use gradle bootRun

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -8,12 +8,41 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
 
 @SpringBootApplication
 public class Application {
 
+   /* @Bean
+    public ITemplateResolver templateResolver() {
+        TemplateResolver resolver = new ServletContextTemplateResolver();
+        resolver.setPrefix("/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setCacheTTLMs(0l);
 
+        return resolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(templateResolver());
+
+        return engine;
+    }
+
+    @Bean
+    public ViewResolver thymeleafViewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setCharacterEncoding("UTF-8");
+
+        return viewResolver;
+    }*/
 
 
     public static void main(String[] args) {
--- src/main/java/application/controllers/HelloController.java
+++ src/main/java/application/controllers/HelloController.java
@@ -1,14 +1,16 @@
 package application.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    @RequestMapping("/helloworld")
+    public String hello(ModelMap map) {
+        return "hello";
     }
 
 }
